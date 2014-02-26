08196dd8b9c8a4d0a7b38a7767c4af2d0c594ebb
monkeyulize <sholberg92@gmail.com>
2014-02-26 15:36:50 -0600

    removed test case from main

--- dbms/dbmsMain/main.cpp
+++ dbms/dbmsMain/main.cpp
@@ -10,27 +10,9 @@ int main()
 {
 	database db;
 	parser p(db);
-	
-	stringstream ss;
-	ss << "CREATE TABLE shapes(shape VARCHAR(20)) PRIMARY KEY(shape);" <<
-		"INSERT INTO shapes VALUES FROM(\"circle\");" <<
-		"INSERT INTO shapes VALUES FROM(\"square\");" <<
-		"INSERT INTO shapes VALUES FROM(\"rectangle\");" <<
-		"INSERT INTO shapes VALUES FROM(\"triangle\");" <<
-		"SHOW shapes;" <<
-		"CREATE TABLE colors(color VARCHAR(20)) PRIMARY KEY(color);" <<
-		"INSERT INTO colors VALUES FROM(\"red\");" <<
-		"INSERT INTO colors VALUES FROM(\"blue\");" <<
-		"SHOW colors;" <<
-		"product_test <-shapes * colors;"<<
-		"SHOW product_test;";
-	cin.rdbuf(ss.rdbuf());
-	p.evaluate_statement();
-	
 
-
-	//Application A(db, p);
-	//A.initialize();
+	Application A(db, p);
+	A.initialize();
 
 	
 	system("pause");
